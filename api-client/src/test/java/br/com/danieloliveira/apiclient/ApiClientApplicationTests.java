474778562ced501812a083d6fdee1677f9aa7f73
Daniel Oliveira <danielso2007@gmail.com>
2024-05-20 12:55:09 -0300

    feat: projeto iniciado

---
+++ api-client/src/test/java/br/com/danieloliveira/apiclient/ApiClientApplicationTests.java
@@ -0,0 +1,15 @@
+package br.com.danieloliveira.apiclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class ApiClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ api-service/src/test/java/br/com/danieloliveira/apiservice/ApiServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package br.com.danieloliveira.apiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class ApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
